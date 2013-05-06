8bb3115595b16b211189149e2e68d5ab82ad8b85
ruben11set@gmail.com <ruben11set@gmail.com@325c74e8-ffaf-15da-9f85-cdbc50d2889d>
2013-05-06 20:26:08 +0000

    first commit

---
+++ trunk/N64BluetoothServer/N64BlueToothServer.py
@@ -0,0 +1,46 @@
+'''
+Created on 03/05/2013
+
+@author: Ruben Miguel Corcoba
+'''
+# file: rfcomm-server.py
+# auth: Albert Huang <albert@csail.mit.edu>
+# desc: simple demonstration of a server application that uses RFCOMM sockets
+#
+# $Id: rfcomm-server.py 518 2007-08-10 07:20:07Z albert $
+
+from bluetooth import *
+
+server_sock=BluetoothSocket( RFCOMM )
+server_sock.bind(("",0))
+server_sock.listen(1)
+
+port = server_sock.getsockname()[1]
+
+uuid = "00001101-0000-1000-8000-00805F9B34FB"
+
+advertise_service( server_sock, "N64BlueToothServer",
+                   service_id = uuid,
+                   service_classes = [ uuid, SERIAL_PORT_CLASS ],
+                   profiles = [ SERIAL_PORT_PROFILE ],
+#                   protocols = [ OBEX_UUID ]
+                    )
+                   
+print "Waiting for connection on RFCOMM channel %d" % port
+
+client_sock, client_info = server_sock.accept()
+print "Accepted connection from ", client_info
+
+try:
+    while True:
+        data = client_sock.recv(1024)
+        if len(data) == 0: break
+        print "received [%s]" % data
+except IOError:
+    pass
+
+print "disconnected"
+
+client_sock.close()
+server_sock.close()
+print "all done"
